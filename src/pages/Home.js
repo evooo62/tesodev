f4cb244ac947bd78d4fec9024423b0b69269abac
evooo62 <evren622@gmail.com>
2022-02-25 23:45:07 +0300

    json data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,62 @@
 import React, { useEffect, useState } from "react";
 import images from "../images/tesodev-img.PNG";
 import Image from "react-bootstrap/Image";
-import { Col, Input, Row, Table } from "antd";
-import ColumnGroup from "antd/lib/table/ColumnGroup";
-import Column from "antd/lib/table/Column";
+import {Col, Input, Row, Table  } from "antd";
+
+
 
 function Home() {
   
  const[list,setList] = useState([]);
+ 
   
   useEffect(async()=>{
     try {
       let json = require('../json/mockData.json');
-      setList([json]);
-      console.log(json.data, 'the json obj');
+      setList([json.data]);
+      console.log(list, 'the json obj');
     } catch (error) {
       console.log('err', error);
     };
+    
   },[]);
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "0",
+      key: "name",
+    },
+    {
+      title: "Company",
+      dataIndex: "1",
+      key: "company",
+    },
+    {
+      title: "Email",
+      dataIndex: "2",
+      key: "email",
+    },
+    {
+      title: "Date",
+      dataIndex: "3",
+      key: "date",
+    },
+    {
+      title: "Country",
+      dataIndex: "4",
+      key: "county",
+    },
+    {
+      title: "City",
+      dataIndex: "5",
+      key: "city",
+    },
+  ];
 
   return (
     <>
-      {/* Hello world */}
+     
       <Row justify="center" align="middle">
         <Col>
           <Image src={images} alt="Girl in a jacket" width={500} height={200} />
@@ -29,22 +64,17 @@ function Home() {
       </Row>
       <Row justify="center" align="middle">
         <Col span={10}>
-          <Input></Input>
+          <Input placeholder="Evren"></Input>
         </Col>
       </Row>
+      <Row justify="center" align="middle">
+        <Col >
+        <Table dataSource={list[0]} columns={columns} />;
+        </Col>
+      </Row>
+    
       
-      <Table>
-          <ColumnGroup>
-          {list.map((item)=>{
-            return (
-              <Column key={item.id}>
-              {item.city}
-              </Column>
-            )
-          })}
-         
-          </ColumnGroup>
-      </Table>
+     
     </>
   );
 }
